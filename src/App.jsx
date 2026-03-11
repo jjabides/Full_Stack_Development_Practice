344189d827a76c08260d478f41f27e2afaf1ccbe
DESKTOP-8KSJMJD\jjabi <jj.abides@hotmail.com>
2026-03-11 11:32:08 -0700

    Revert "add server-side rendering"
    This reverts commit c7d27a2666fbf5bd5933d146924fc2774edec6fc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Blog } from './pages/Blog.jsx'
+import { Signup } from './pages/Signup.jsx'
+import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import PropTypes from 'prop-types'
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Blog />,
+    },
+    {
+        path: '/signup',
+        element: <Signup />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+])
 
-export function App({ children }) {
+export function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <AuthContextProvider>{children}</AuthContextProvider>
+            <AuthContextProvider>
+                <RouterProvider router={router} />
+            </AuthContextProvider>
         </QueryClientProvider>
     )
-}
-
-App.propTypes = {
-    children: PropTypes.element.isRequired,
 }
