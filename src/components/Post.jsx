5f2efda7dab3035248a8598adb52780b895f333a
DESKTOP-8KSJMJD\jjabi <jj.abides@hotmail.com>
2026-03-11 14:40:59 -0700

    fix linting issues

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types'
 import { User } from './User.jsx'
+import { Link } from 'react-router-dom'
 
-export function Post({ title, contents, author }) {
+export function Post({ title, contents, author, _id, fullPost = false }) {
     return (
         <article>
-            <h3>{title}</h3>
-            <div>{contents}</div>
+            {fullPost ? (
+                <h3>{title}</h3>
+            ) : (
+                <Link to={`/posts/${_id}`}>
+                    <h3>{title}</h3>
+                </Link>
+            )}
+            {fullPost && <div>{contents}</div>}
             {author && (
                 <em>
-                    <br />
+                    {fullPost && <br />}
                     Written by <User id={author}></User>
                 </em>
             )}
@@ -20,4 +27,6 @@ Post.propTypes = {
     title: PropTypes.string.isRequired,
     contents: PropTypes.string,
     author: PropTypes.string,
+    _id: PropTypes.string.isRequired,
+    fullPost: PropTypes.bool,
 }
